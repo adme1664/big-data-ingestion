34a07e4fc748e4f2d02461f29940c3aebb198d62
adme1664 <admejeanjeff@gmail.com>
2026-07-01 17:25:39 -0400

    fix test

--- src/test/java/com/adme/learning/data/ingestion/services/ManageEmployeeServiceTest.java
+++ src/test/java/com/adme/learning/data/ingestion/services/ManageEmployeeServiceTest.java
@@ -58,7 +58,7 @@ class ManageEmployeeServiceTest {
         assertThat(result).hasSize(1);
         EmployeeDto dto = result.get(0);
         assertThat(dto.id()).isEqualTo("1");
-        assertThat(dto.employeeNo()).isEqualTo("1234");
+        assertThat(dto.employeeNo()).isEqualTo("7369");
         assertThat(dto.fullName()).isEqualTo("SMITH");
         assertThat(dto.position()).isEqualTo("CLERK");
         assertThat(dto.managerNo()).isEqualTo("7902");
